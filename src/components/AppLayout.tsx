a1374fb3ccfe0aee22d21b77433289d353becd4a
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2025-01-08 23:49:33 +0900

    アプリタイトル変更

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -59,7 +59,7 @@ export default function AppLayout() {
             <MenuIcon />
           </IconButton>
           <Typography variant='h6' noWrap component='div'>
-            Responsive drawer
+            家計簿
           </Typography>
         </Toolbar>
       </AppBar>
